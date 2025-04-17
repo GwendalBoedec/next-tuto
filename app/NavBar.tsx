757486a79df71dfe0db46c8d1babb12fadc790b4
Gwendal Boedec <boedec.g@gmail.com>
2025-04-17 18:44:32 +0200

    add authentication

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,11 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
+import { useSession } from 'next-auth/react'
 
 const NavBar = () => {
+  const {status, data: session} = useSession();
+
   return (
     <nav >
           <ul className="flex p-2 bg-slate-200">
@@ -17,6 +21,15 @@ const NavBar = () => {
             <li className='mr-5'>
             <Link href="/admin"> Admin </Link>
             </li>
+            {status === "loading" && <div>Loading...</div>}
+            { status === "authenticated" && 
+            <div> 
+              {session.user!.name} 
+              <Link className="ml-3" href="/api/auth/signout"><button className='btn btn-second'>Sign Out</button></Link>
+            </div>}
+            { status === "unauthenticated" && <li className='mr-5'>
+            <Link href="/api/auth/signin"> Login </Link>
+            </li> }
           </ul>
         </nav>
   )
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,45 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/prisma/client";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+ 
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: { label: "Password", type: "password", placeholder: "password" },
+      },
+      async authorize(credentials) {
+        const creds = credentials as { email: string; password: string };
+        if (!creds?.email || !creds.password) return null;
+       const user = await prisma.user.findUnique({ 
+        where: { email: creds.email },
+        select: {
+          id: true,
+          email: true,
+          hashedPassword: true,
+        },
+        });
+        if (!user || !user.hashedPassword) return null;
+        const passwordMatch = await bcrypt.compare(creds.password, user.hashedPassword!);
+        return passwordMatch ? user : null;
+      },
+    }),
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+      })
+    ],
+  session: {
+    strategy: "jwt"
+  }
+}
 
-const handler = NextAuth({});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST }
