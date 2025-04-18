dd3652f5ed5ac77f5dd5599f4a293dab9e9b304b
Gwendal Boedec <boedec.g@gmail.com>
2025-04-18 13:54:41 +0200

    add email template

--- emails/welcomeTemplate.tsx
+++ emails/welcomeTemplate.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { Html, Body, Container, Text, Link, Preview} from "@react-email/components"
+import React, { CSSProperties } from 'react'
+import { Html, Body, Container, Text, Link, Preview, Tailwind } from "@react-email/components"
 
-const welcomeTemplate = ({name} : {name: string}) => {
-  return (
-    <Html>
-        <Preview> Welcome aboard! </Preview>
-        <Body>
-            <Container>
-                <Text>Hello {name}</Text>
-                <Link href='https://codewithmosh.com'>check our website</Link>
-            </Container>
-        </Body>
-    </Html>
-  )
+const welcomeTemplate = ({ name }: { name: string }) => {
+    return (
+        <Html>
+            <Preview> Welcome aboard! </Preview>
+            <Tailwind>
+                <Body className="bg-white">
+                    <Container>
+                        <Text className="font-bold text-3xl">Hello {name}</Text>
+                        <Link href='https://codewithmosh.com'>check our website</Link>
+                    </Container>
+                </Body>
+            </Tailwind>
+        </Html>
+    )
+}
+
+const body: CSSProperties = {
+    background: "#fff"
+}
+
+const heading: CSSProperties = {
+    fontSize: "32px"
 }
 
 export default welcomeTemplate
